b360eb7a964b4e8a44984ac06c29319f1dd7ce23
unosvk001 <kattojuvk@gmail.com>
2024-10-27 12:55:59 +0530

    Updates

---
+++ precache-manifest.6bf49e38dae78834853be57c42b52347.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d99fc9c4bd4612310491d011a791463",
+    "url": "%3Cyour-github-username%3E.github.io/my-portfolio/index.html"
+  },
+  {
+    "revision": "936aba6686f635417e23",
+    "url": "%3Cyour-github-username%3E.github.io/my-portfolio/static/css/main.e8c013c4.chunk.css"
+  },
+  {
+    "revision": "a085feaa7aeaa4b94a1b",
+    "url": "%3Cyour-github-username%3E.github.io/my-portfolio/static/js/2.49c19acb.chunk.js"
+  },
+  {
+    "revision": "60c46b200b0cfd41617f",
+    "url": "%3Cyour-github-username%3E.github.io/my-portfolio/static/js/3.b265e9c7.chunk.js"
+  },
+  {
+    "revision": "936aba6686f635417e23",
+    "url": "%3Cyour-github-username%3E.github.io/my-portfolio/static/js/main.17d9a1a1.chunk.js"
+  },
+  {
+    "revision": "be1db231c48054e485ee",
+    "url": "%3Cyour-github-username%3E.github.io/my-portfolio/static/js/runtime~main.549ec1d1.js"
+  }
+]);
